1fbdd93c0c6c9a479428a7e8f80c2ed0e4d81ef5
Jordi Isken <72348117+UBONGFX@users.noreply.github.com>
2025-04-11 19:29:36 +0200

    added frist draft of my zsh files and my current starship conf

---
+++ zsh/aliases.zsh
@@ -0,0 +1,12 @@
+alias gg='git log --all --decorate --oneline --graph'
+alias gs='git status'
+alias gp='git pull'
+
+alias ll='ls -lah -g --icons'
+alias la='eza -l -a -g --icons'
+alias ls='eza --icons'
+
+alias k='kubectl'
+alias t='terraform'
+alias h='helm'
+alias a='ansible'
