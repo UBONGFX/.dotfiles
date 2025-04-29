3809247cc27d18b816ff21b1f9463b8e4fa0e021
Jordi Isken <jordi.isken@volkswagen.de>
2025-04-29 15:02:15 +0200

    added new git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,8 +3,10 @@ echo "🚧 Loading aliases"
 alias gg='git log --all --decorate --oneline --graph'
 alias gs='git status'
 alias gc='git commit'
+alias gb='git branch'
 alias gf='git fetch'
 alias gp='git pull'
+alias ga='git add'
 
 alias ll='ls -lah -g --icons --git'
 alias la='eza -l -a -g --icons --git'
