6164bc8e9b5da3b4f64c5795bc8cdc6559453390
Jordi Isken <jordi.isken@volkswagen.de>
2025-04-29 15:05:03 +0200

    minor fixes

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -4,17 +4,16 @@ echo "🚧 Loading completions"
 autoload -Uz compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 
-# Helm completion (if installed)
+# Loading completion (if installed)
+
 if command -v helm >/dev/null 2>&1; then
   source <(helm completion zsh)
 fi
 
-# Kubectl (Kubernetes) completion (if installed)
 if command -v kubectl >/dev/null 2>&1; then
   source <(kubectl completion zsh)
 fi
 
-# Terraform completion (if installed)
 if command -v terraform >/dev/null 2>&1; then
   complete -o nospace -C /opt/homebrew/bin/terraform terraform
 fi
